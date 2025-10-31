e0ffad65605cb6434e1318fcd4ad10f754fc41d6
Ruslan Kasinets <rulik.357@gmail.com>
2025-10-31 19:14:37 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((elem) => {
+    if (!elem.id) {
+      elem.id = elem.name || elem.type;
+    }
+
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+    label.htmlFor = elem.id;
+    elem.insertAdjacentElement('beforebegin', label);
+
+    const labelText = normalizeName(elem.name);
+
+    label.textContent = labelText;
+    elem.placeholder = labelText;
+  });
+});
+
+function normalizeName(text) {
+  const normalizedName = '' + text[0].toUpperCase() + text.slice(1);
+
+  return normalizedName;
+}
